1a150607a65397626b0577087b65ac0959376c14
dextroxd <divyanshu171299@gmail.com>
2019-03-25 06:51:24 +0530

    New modifications

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,14 +7,34 @@ class Home extends StatelessWidget {
     return new Container(
       color: Colors.greenAccent,
       alignment: Alignment.center,
-      child: new Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Text("First Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
-        new Text("Second Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
-        new Text("Third Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
-        new Text("Fourth Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
-        new Container(color: Colors.deepOrange.shade50,alignment: Alignment.bottomLeft,child:new Text("Fifth Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),)
-      ],)
+child: new Stack(
+  alignment: Alignment.bottomCenter,
+  children: <Widget>[
+    const Text("Hello there",),
+    const Text("Heay again",),
+    const Text("DD"),
+  ],
+),
+//      child: new Row(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: <Widget>[
+//          new Text("Hello",textDirection: TextDirection.ltr,
+//          style: new TextStyle(fontSize: 12.9,),),
+//          new Text("Hello",textDirection: TextDirection.ltr,style: new TextStyle(fontSize: 12.9,),),
+//          new Text("Hello",textDirection: TextDirection.ltr,style: new TextStyle(fontSize: 12.9,),),
+//          const Expanded(
+//            child: const Text("Hello"),
+//          )
+//        ],
+//      ),
+//      child: new Column(mainAxisAlignment: MainAxisAlignment.center,
+//      children: <Widget>[
+//        new Text("First Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
+//        new Text("Second Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
+//        new Text("Third Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
+//        new Text("Fourth Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),
+//        new Container(color: Colors.deepOrange.shade50,alignment: Alignment.bottomLeft,child:new Text("Fifth Item",textDirection: TextDirection.ltr,style: new TextStyle(color: Colors.white),),)
+//      ],)
 //      child: new Text(
 //        "Hello Container",
 //        textDirection: TextDirection.ltr,
